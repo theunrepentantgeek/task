ba22eacd7a267243f707df5e828608f57592a95d
Bevan Arps <bevan.arps@outlook.com>
2023-06-28 19:23:57 +1200

    Add styling to nodes

--- cmd/task-graph/node.go
+++ cmd/task-graph/node.go
@@ -1,22 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"io"
 )
 
 type node struct {
-	id    string
-	label string
+	id         string
+	label      string
+	attributes Attributes
 }
 
 func NewNode(id string, label string) *node {
-	return &node{
-		id:    id,
-		label: label,
+	result := &node{
+		id:         id,
+		label:      label,
+		attributes: make(Attributes),
+	}
+
+	result.attributes["label"] = label
+
+	return result
+}
+
+func (n *node) applyStyle(style Attributes) {
+	for k, v := range style {
+		n.attributes[k] = v
 	}
 }
 
 func (n *node) writeTo(buffer io.StringWriter) {
-	buffer.WriteString(fmt.Sprintf("    %s [label=%q];\n", n.id, n.label))
+	n.attributes.writeTo(n.id, buffer)
 }
